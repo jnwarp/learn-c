bd7dd5d8fbde74ace7a2d44e6b25bb6c89b47d58
James Warp <j.newtonwarp@gmail.com>
2017-01-19 13:44:10 -0500

    Add arrays example

---
+++ src/arrays.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int numbers[10];
+
+	numbers[0] = 10;
+	numbers[1] = 20;
+	numbers[2] = 30;
+	numbers[3] = 40;
+
+	printf("The fourth number in the array is %d", numbers[3]);
+
+	return 0;
+}
